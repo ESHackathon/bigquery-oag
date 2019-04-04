7d9c9f2a044a8736abf6a7faff7e16807078c59b
Daniel Pérez Rada <dperezrada@gmail.com>
2019-04-05 10:04:25 +1100

    FIX: google storage location

--- bigquery_upload_linking.sh
+++ bigquery_upload_linking.sh
@@ -4,13 +4,13 @@
 MAX_ERRORS=10000
 
 TABLE=opensource-202114:openacademicgraph.papers_linking
-echo "mag_papers_${i}"
-bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/paper_linking_pairs.txt
+echo "papers_linking"
+bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/paper_linking_pairs.txt.gz
 
 TABLE=opensource-202114:openacademicgraph.venues_linking
-echo "mag_venues_${i}"
-bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/venue_linking_pairs.txt
+echo "venues_linking"
+bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/venue_linking_pairs.txt.gz
 
 TABLE=opensource-202114:openacademicgraph.authors_linking
-echo "mag_authors_${i}"
-bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/author_linking_pairs.txt
+echo "authors_linking"
+bq load --autodetect --noreplace --max_bad_records=${MAX_ERRORS} --source_format=NEWLINE_DELIMITED_JSON ${TABLE} gs://osgv2/raw/author_linking_pairs.txt.gz
